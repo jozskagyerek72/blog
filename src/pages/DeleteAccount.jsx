612c0f40c3217ab649d19f4d56a50cf247923287
jozskagyerek72 <gillichbalint@gmail.com>
2024-12-12 16:08:08 +0100

    delete account update

--- src/pages/DeleteAccount.jsx
+++ src/pages/DeleteAccount.jsx
@@ -3,11 +3,18 @@ import { middleStyle } from '../utils/utils'
 import { useContext } from 'react'
 import { userContext } from '../context/UserContext'
 import { useConfirm } from 'material-ui-confirm'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const DeleteAccount = () => {
     
     const {user, logOutUser, deleteAccount} = useContext(userContext)
+    const navigate = useNavigate()
     const confirm = useConfirm()
+
+    useEffect(()=>{
+        !user && navigate("/")
+    },[user])
 
     const handleDel = async () =>
     {
@@ -20,7 +27,7 @@ export const DeleteAccount = () => {
             })
             await deleteAccount()
             logOutUser()
-            
+
         } catch (error) {
             console.log("cancel: ", error);
             
